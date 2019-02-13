42191061d328cb099639300a26c2f467d4eb27ac
S.SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-02-13 10:16:07 -0800

    Update example 3.2 luc.c

--- example 3.2 luc.c
+++ example 3.2 luc.c
@@ -1,7 +1,7 @@
-/* In a company an employee is paid a udner:
-if has vasic salary is less than Rs. 1500, then HRA=10% of basic salary and DA=90% of basic salary. If 
-his salary is either equal to or above Rs. 1500, then HRA =Rs,500 and DA=98% of basic salary. if the 
-employee's slary is input through the keyboard write a program to find his gross salary.   */
+/* In a company an employee is paid as under:
+if his basic salary is less than Rs. 1500, then HRA=10% of basic salary and DA=90% of basic salary. If his 
+salary is either equal to or above Rs. 1500, then HRA =Rs,500 and DA=98% of basic salary. if the employee's 
+salary is input through the keyboard write a program to find his gross salary.   */
 
 /*Calculation of Gross salry */
 #include<stdio.h>
