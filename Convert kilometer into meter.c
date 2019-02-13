a4eae5216a7615c0444dc69fb5c31c5b9ce109f9
S.SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-02-13 09:28:07 -0800

    Convert kilometer into meter
    Program to convert Kilometer into meter.

---
+++ Convert kilometer into meter.c
@@ -0,0 +1,13 @@
+/* Program to convert Kilometer into meter.
+SUMIT  KUMAR          */
+#include<stdio.h>
+int main(void)
+{
+	int km, m; 
+	km=25;                           /*input for kilometer */
+	m=km*1000;                       /*findidng the meter */
+	printf("The meter is=%d", m);    /*printing the meter */
+	return 0;
+}
+
+/*   OUTPUT: The meter is=25000     */ 
