896fb2c8d5d15209aa077009709db964563e315e
S.SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-02-13 09:26:10 -0800

    Student Deatils
    Program to print the name, brach, college, roll no, reg No. using printf function.

---
+++ Student details.c
@@ -0,0 +1,21 @@
+/* Program to print the name, brach, college, roll no, reg No. using printf function.    
+ SUMIT KUMAR        */
+    #include<stdio.h>
+    int main(void)
+    {
+    	printf("\n Name      : Sumit");
+    	printf("\n Branch    : CSE");
+    	printf("\n College   : NCE");
+    	printf("\n Age       : 18");
+    	printf("\n Roll No   : 17CS58");
+    	printf("\n Reg No    : 1710510904");
+    	
+    	return 0;
+	}
+	
+	/* OUTPUT:  Name      : Sumit
+    	        Branch    : CSE
+    	        College   : NCE
+    	        Age       : 18
+    	        Roll No   : 17CS58
+    	        Reg No    : 1710510904        */
