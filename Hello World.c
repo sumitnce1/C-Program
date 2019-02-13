a46c689678ba608ffc6d5f0c2c4c0a57818fbd83
S.SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-02-13 09:25:25 -0800

    Hello World
    Program to print the "Hello World"

---
+++ Hello World.c
@@ -0,0 +1,12 @@
+    /* 1. Program to print the "Hello World"
+    SUMIT KUMAR        */
+    #include<stdio.h>
+    int main(void)
+    {
+    	printf("Hello World\n");
+    	return 0;
+	}
+	
+	
+	
+	/* OUTPUT: Hello World      */
