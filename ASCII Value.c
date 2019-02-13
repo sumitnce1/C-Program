f8c959fa73abc6940fe4f3cd6a2a49f4ba5d64fa
S.SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-02-13 09:26:59 -0800

    ASCII Value
    Program to print the ASCII value of the character

---
+++ ASCII Value.c
@@ -0,0 +1,19 @@
+/* Progsram to print the ASCII value of the character  
+SUMIT KUMAR      */
+#include<stdio.h>
+int main(void)
+{
+	char first = 'N';
+	char second = 92;
+	printf("\nThe first example as a letter looks like this = %c", first);
+	printf("\nThe first example as a letter looks like this = %d", first);
+	printf("\nThe second example as a letter looks like this = %c", second);
+	printf("\nThe second example as a letter looks like this = %d", second);
+	
+	return 0;
+}
+
+/* OUTPUT: The first example as a letter looks like this = N
+           The first example as a letter looks like this = 78
+           The second example as a letter looks like this = a
+           The second example as a letter looks like this = 97     */
