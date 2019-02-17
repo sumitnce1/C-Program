035e7b88a1b36338904cf58cb7a177acb6593c5a
S.SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-02-18 00:13:42 +0530

    Arithmatic  operators
    C Program to demonstrate the working of arithmetic operators

---
+++ Arthmatic operators.c
@@ -0,0 +1,20 @@
+/* // C Program to demonstrate the working of arithmetic operators
+SUMIT KUMAR           */
+
+#include <stdio.h>
+int main()
+{
+    int a = 9,b = 4, c;
+        c = a+b;
+    printf("a+b = %d \n",c);
+    c = a-b;
+    printf("a-b = %d \n",c);
+        c = a*b;
+    printf("a*b = %d \n",c);
+        c=a/b;
+    printf("a/b = %d \n",c);
+        c=a%b;
+    printf("Remainder when a divided by b = %d \n",c);
+    
+    return 0;
+}
