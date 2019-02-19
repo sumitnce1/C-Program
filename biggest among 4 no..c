4e21954fbdfeb6beb3a62d5a1ff24157bbe3d90a
S.SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-02-19 23:02:07 +0530

    biggest among 4 no.
    Program to find the biggest among 4 numbers

---
+++ biggest among 4 no..c
@@ -0,0 +1,14 @@
+/* Program to a number is even or odd
+SUMIT KUMAR              */
+#include<stdio.h>
+int main(void)
+{
+	int a;
+	printf("\nEnter a number : ");
+	scanf("%d", &a)
+	if (a %2==0)
+	printf("\nThe %d is even.",a);
+	else
+	printf("\nThe %d is odd.",a);
+	return 0;
+}
