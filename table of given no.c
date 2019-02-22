479ff680d814428f404ba75bfd8fb7daf72ac8d7
S.SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-02-23 00:06:04 +0530

    table of given no
    Program is to print the table of a given number.

---
+++ table of given no.c
@@ -0,0 +1,14 @@
+     /* Program is to print the table of a given number.
+     SUMIT KUMAR    */
+     #include<stdio.h>
+     int main(Void)
+     {
+     	int number, i;
+     	printf("\n Enter a number to the Table : ");
+     	scanf("%d",&number);
+     	printf("\n The %dth Table is",number);
+     	for(i=1;i<=10;i++);
+     	printf("\n\t %d * %d =%d",number,i,number*i);
+     	
+		 return 0;
+	 }
