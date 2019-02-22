3013b7b307b1138217d682ca23a4fb18e65d1764
S.SUMIT <40803138+sumitnce1@users.noreply.github.com>
2019-02-23 00:03:36 +0530

    Second largest 3no
    Program to find the largest and second largest of three numbers

---
+++ Second largest 3no.c.c
@@ -0,0 +1,17 @@
+/* Program to find the largest and second largest of three numbers 
+SUMIT KUMAR   */
+#include<stdio.h>
+#define BIG(a,b) ((a>b)?(a):(b))
+#define SAM(a,b) ((a<b)?(a):(b))
+int main(void)
+{
+	int n,m,o,lar,sma,seclar;
+	printf("Enter the three numbers: ");
+	scanf("%d%d%d", &n,&m,&o)
+	lar= BIG((BIG(n,m)),o);
+	sma= SAM((SAM(n,m)),o);
+	seclar=(n+m+o)-(lar+sma);
+	printf("Largest of three numbers is %d\n", lar);
+	printf("Second Largest of three numbers is %d",seclar);
+	return 0;
+}
